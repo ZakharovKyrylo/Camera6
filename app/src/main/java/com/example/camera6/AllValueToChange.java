674d28834c10769e1e33c94f9c24b57220e40ec4
Kyrylo <zakharovkyrylo@gmail.com>
2022-09-09 15:01:30 +0300

    add 2 classes time 8 sec to do!

---
+++ app/src/main/java/com/example/camera6/AllValueToChange.java
@@ -0,0 +1,13 @@
+package com.example.camera6;
+
+public class AllValueToChange {
+    public static final String myLog = "My Log - ";
+    public static final String mCameraID = "0"; // выбираем какую камеру использовать 0 - основная, 1 - фронтальная
+    public static final String folderName = "DETECTION";
+
+    public static final int recTime = 8 * 1000; // 2 * 60 * 1000
+    public static final int screenDelay = 1000; // = 1 sec
+    public static final int maxDetection =11;
+    public static final int widthScreen =160;
+    public static final int heightScreen =120;
+}
---
+++ app/src/main/java/com/example/camera6/CreateMyFile.java
@@ -0,0 +1,72 @@
+package com.example.camera6;
+
+import android.media.CamcorderProfile;
+import android.media.MediaRecorder;
+import android.os.Build;
+import android.os.Environment;
+
+import java.io.File;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+import static com.example.camera6.AllValueToChange.*;
+
+public class CreateMyFile {
+    private File mCurrentFile;
+    private MediaRecorder mMediaRecorder;
+
+    public CreateMyFile(MediaRecorder mMediaRecorder ){
+        this.mMediaRecorder = mMediaRecorder;
+    }
+
+    public void setUpMediaRecorder() {
+        mMediaRecorder = new MediaRecorder();
+        mMediaRecorder.setVideoSource(MediaRecorder.VideoSource.SURFACE);
+        mMediaRecorder.setOutputFormat(MediaRecorder.OutputFormat.MPEG_4);
+        mCurrentFile = new File(createDirectory(folderName), generateFileName());
+        mMediaRecorder.setOutputFile(mCurrentFile.getAbsolutePath());
+        CamcorderProfile profile = CamcorderProfile.get(CamcorderProfile.QUALITY_480P);
+        mMediaRecorder.setVideoFrameRate(profile.videoFrameRate);
+        mMediaRecorder.setVideoSize(profile.videoFrameWidth, profile.videoFrameHeight);
+        mMediaRecorder.setVideoEncodingBitRate(profile.videoBitRate);
+        mMediaRecorder.setVideoEncoder(MediaRecorder.VideoEncoder.H264);
+        try {
+            mMediaRecorder.prepare();
+        } catch (Exception e) {
+            mCurrentFile.delete();
+            setUpMediaRecorder();
+        }
+    }
+
+    public MediaRecorder getMediaRecorder(){
+        return mMediaRecorder;
+    }
+    // генерация имени файла
+    private String generateFileName() { // название файла в виде дата,месяц,год_час,минута,секунда
+        Date dateNow = new Date();//("yyyy.MM.dd 'и время' hh:mm:ss a zzz");
+        SimpleDateFormat formatForDateNow = new SimpleDateFormat("dd.MM.yyyy_hh:mm:ss");
+        return ("" + formatForDateNow.format(dateNow) + ".mp4");
+    }
+    //создание папки для видео
+    private File createDirectory(String name) {
+        File baseDir;
+        if (Build.VERSION.SDK_INT < 8) {
+            baseDir = Environment.getExternalStorageDirectory();
+        } else {
+            baseDir = Environment.getExternalStoragePublicDirectory(Environment.DIRECTORY_DCIM);
+        }
+        if (baseDir == null)
+            return Environment.getExternalStorageDirectory();
+        File folder = new File(baseDir, name);
+        if (folder.exists()) {
+            return folder;
+        }
+        if (folder.isFile()) {
+            folder.delete();
+        }
+        if (folder.mkdirs()) {
+            return folder;
+        }
+        return Environment.getExternalStorageDirectory();
+    }
+}
